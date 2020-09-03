5208ebaaa5a6c4efd062ecb01b2801d036a4ac18
ybguo1 <a5177899b@gmail.com>
2020-09-03 04:06:38 +0100

    Add files via upload

---
+++ segmentation/im_rotation.m
@@ -0,0 +1,17 @@
+function center_patch = im_rotation(im, patch_size, angle)
+
+im=double(im);
+[m,n,c]=size(im);
+center=(m+1)/2;
+angle=angle/180*pi;
+
+[x,y]   = meshgrid(1:m,1:m);
+rotation_matrix = [cos(angle) sin(angle);-sin(angle) cos(angle)];
+all_coords = rotation_matrix*[x(:)-center,y(:)-center]';
+
+for i=1:3
+    rpatch = interp2(im(:,:,i), all_coords(1,:)+center,all_coords(2,:)+center, 'linear');
+    rim= reshape(rpatch,[m,m]);
+    center_patch(:,:,i)=rim(center-(patch_size-1)/2:center+(patch_size-1)/2,center-(patch_size-1)/2:center+(patch_size-1)/2);
+end
+
